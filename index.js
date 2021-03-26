cea798cea0c4a2d2cc11d9e2064a10e69a87e5e0
ili2908 <38957777+ili2908@users.noreply.github.com>
2021-03-26 20:16:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,13 @@
 //}
 
  window.addEventListener('load', function () {
-			console.log(window);
+			
 			if (window.ethereum) {
 				window.web3 = new Web3(window.ethereum);
 				window.ethereum.enable();
 				return true;
 			}
+	 		console.log(window);
 			return false;
  });
 function minus(event){
